ec147e6112e22c3daef39c7b35d622d41203f642
sepsimus <kostya2120@yandex.ru>
2024-01-28 22:37:11 +0300

    upd: cover section add social icon

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -6,6 +6,9 @@ function Cover() {
             <h1 className='cover__title'>AskGskGo — ваш путеводитель в мир исцеления, оздоровления и долголетия</h1>
             <p className='cover__subtitle'>Создайте лучшую версию себя</p>
             <button className='cover__button'>Получить бесплатную консультацию</button>
+            <a className='cover__social cover__social_tel' href='#'></a>
+            <a className='cover__social cover__social_whats-up' href='#'></a>
+            <a className='cover__social cover__social_mes' href='#'></a>
         </section>
     )
 };
